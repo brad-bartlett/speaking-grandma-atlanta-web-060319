bef2ac9b662dd56bf36cfd991ac88c594a2e78f9
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-05 06:29:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,12 +10,12 @@ def speak_to_grandma(phrase)
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
   elsif phrase == phrase.downcase
-    return "HUH? SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.upcase
+    return "HUH? SPEAK UP, SONNY!"
 # NO, NOT SINCE 1938!
   else
-    return "NO, NOT SINCE 1938!"
+    return "HUH? SPEAK UP, SONNY!"
   end
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
