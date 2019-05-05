641f2271f535ca4d2e2a79ce8a96b32c62e3baf0
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-05 06:26:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
   else
     return "NO, NOT SINCE 1938!"
   end
-  
+
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 end
